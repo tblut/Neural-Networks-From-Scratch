bug fixes, softmax layer, mnist dataset loading

nnfs/layers.py
@@ -42,5 +42,19 @@ class ReLU:
         return np.maximum(0.0, inputs)
 
     def backward(self, grad_out):
-        grad_in = np.where(self._inputs > 0, 1.0, 0.0)
+        grad_in = np.where(self._inputs < 0.0, 0.0, 1.0)
         return grad_out * grad_in
+
+
+class Softmax:
+    def forward(self, inputs):
+        maxs = np.max(inputs, axis=1).reshape((-1, 1))
+        exps = np.exp(inputs - maxs)
+        self._outputs = exps / np.sum(exps, axis=1).reshape((-1, 1))
+        return self._outputs
+
+    def backward(self, grad_out):
+        a = np.empty((grad_out.shape[0], 1))
+        for i in range(a.shape[0]):
+            a[i, :] = np.dot(grad_out[i, :], self._outputs[i, :])
+        return self._outputs * (grad_out - a)

nnfs/losses.py
@@ -6,7 +6,7 @@ _EPS = 1.0e-7
 
 class MSELoss:
     def __call__(self, preds, targets):
-        return np.mean(np.square(preds - targets))
+        return np.mean(np.sum(np.square(preds - targets), axis=1))
 
     def get_grad_in(self, preds, targets):
         return 2.0 * (preds - targets) / preds.shape[0]
@@ -18,3 +18,11 @@ class BinaryCrossEntropyLoss:
 
     def get_grad_in(self, preds, targets):
         return ((1.0 - targets) / (1.0 - preds + _EPS) - targets / (preds + _EPS)) / preds.shape[0]
+
+
+class CrossEntropyLoss:
+    def __call__(self, preds, targets):
+        return -np.mean(np.sum(targets * np.log(preds + _EPS), axis=1))
+
+    def get_grad_in(self, preds, targets):
+        return -targets / (preds.shape[0] * (preds + _EPS))

nnfs/utils.py
@@ -1,12 +1,45 @@
 import numpy as np
+import math
+import tarfile
+import gzip
+import shutil
+from pathlib import Path
+from zipfile import ZipFile
+from urllib.request import urlopen
+from urllib.parse import urlparse
 from nnfs.layers import Linear
 
 
-def gen_xor_data(samples):
-    X = np.random.uniform(-5, 5, (samples, 2)).astype(np.float32)
-    y = [int(x[0] * x[1] > 0.0) for x in X]
-    y = np.array(y, dtype=np.int32).reshape((len(y), 1))
-    return X, y
+def to_one_hot(labels, n_classes=None):
+    return np.identity(n_classes or labels.max() + 1)[labels.reshape(-1)]
+
+
+def download_file(url, target_dir, extract=None, verbose=1):
+    Path(target_dir).mkdir(parents=True, exist_ok=True)
+    filename = Path(urlparse(url).path).name
+    target_path = Path(target_dir, filename)
+    if not target_path.exists():
+        print(f"Downloading {url} ...")
+        with urlopen(url) as response:
+            with open(target_path, 'wb') as file:
+                file.write(response.read())
+
+    if extract:
+        print(f"Extracting {target_path} ...")
+        target_path = Path(target_path)
+        extract_path = Path(target_dir, target_path.stem)
+        if target_path.suffix == '.zip':
+            with ZipFile(target_path, 'r') as zip_file:
+                zip_file.extractall(extract_path)
+        elif '.tar' in target_path.suffixes:
+            with tarfile.open(target_path) as tar:
+                tar.extractall(extract_path)
+        elif target_path.suffix == '.gz':
+            with gzip.open(target_path, 'rb') as file_in:
+                with open(extract_path, 'wb') as file_out:
+                    shutil.copyfileobj(file_in, file_out)
+        
+    return str(extract_path) if extract else target_path
 
 
 def split_train_test(*arrays, test_size=None, train_size=None, shuffle=True, random_seed=None):
@@ -86,8 +119,9 @@ def compute_finite_grad(X, y, model, layer, eps=0.001):
 
 def check_gradients(model, batch_size=4, eps=0.0001):
     n_inputs = model.layers[0].weights.shape[0]
+    n_outputs = model.layers[-2].weights.shape[1]
     X = np.random.rand(batch_size, n_inputs)
-    y = np.ones((batch_size, 1))
+    y = np.identity(n_outputs)[np.random.random_integers(0, n_outputs-1, batch_size)]
 
     n_params = 0
     for layer in model.layers:

train.py
@@ -1,51 +1,56 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from nnfs.model import Model
-from nnfs.layers import Linear, Sigmoid, ReLU
-from nnfs.losses import MSELoss, BinaryCrossEntropyLoss
+from nnfs.layers import Linear, Sigmoid, ReLU, Softmax
+from nnfs.losses import MSELoss, BinaryCrossEntropyLoss, CrossEntropyLoss
 from nnfs.optimizers import SGD
-from nnfs.utils import gen_xor_data, split_train_test, check_gradients
-from nnfs.initializers import he_normal, ones, zeros
-from nnfs.metrics import binary_accuracy
+from nnfs.utils import to_one_hot, split_train_test, check_gradients, download_file
+from nnfs.initializers import *
+from nnfs.metrics import binary_accuracy, multi_class_accuracy
+from nnfs.datasets import mnist, xor
 
+import time
 
 if __name__ == "__main__":
     np.random.seed(42)
+    
+    (X_train_full, y_train_full), (X_test, y_test) = mnist.load_data()
+    y_train_full = to_one_hot(y_train_full)
+    y_test = to_one_hot(y_test)
 
-    n_samples = 4096
-    X, y = gen_xor_data(n_samples)
+    #n_samples = 4096
+    #X, y = xor.generate_data(n_samples)
     #X = np.concatenate([X, (X[:,0] * X[:,1]).reshape((n_samples,1))], axis=1)
     #X = (X[:,0] * X[:,1]).reshape((n_samples,1))
-    X_train_full, X_test, y_train_full, y_test = split_train_test(X, y, test_size=0.2)
+    #X_train_full, X_test, y_train_full, y_test = split_train_test(X, y, test_size=0.2)
     X_train, X_valid, y_train, y_valid = split_train_test(X_train_full, y_train_full, test_size=0.2)
     print(len(X_train), len(y_train), len(X_test), len(y_test))
 
     layers = [
-        Linear(X.shape[1], 4, weights_inititalizer=he_normal),
+        Linear(X_train.shape[1], 16, weights_inititalizer=he_normal),
         ReLU(),
-        Linear(4, 4, weights_inititalizer=he_normal),
+        Linear(16, 16, weights_inititalizer=he_normal),
         ReLU(),
-        Linear(4, 1, weights_inititalizer=he_normal),
-        Sigmoid()
+        Linear(16, y_train.shape[1], weights_inititalizer=he_normal),
+        Softmax()
     ]
     model = Model(layers=layers,
-                  loss=BinaryCrossEntropyLoss(),
-                  optimizer=SGD(lr=0.01))
+                  loss=CrossEntropyLoss(),
+                  optimizer=SGD(lr=0.1))
     history = model.train(X_train, y_train,
-                          epochs=200, batch_size=32,
+                          epochs=100, batch_size=600,
                           validation_data=(X_valid, y_valid),
-                          metrics={'acc': binary_accuracy},
+                          metrics={'acc': multi_class_accuracy},
                           verbose=True)
     
-    #check_gradients(model, batch_size=8, eps=0.0001)
+    #check_gradients(model, batch_size=7, eps=0.0001)
 
-    y_pred = model.predict(X_test)
-    print("test_acc:", binary_accuracy(y_pred, y_test.astype(np.int32)))
+    print("test_accuracy:", multi_class_accuracy(model.predict(X_test), y_test.astype(np.int32)))
 
-    #plt.scatter(X[:, 0], X[:, 1], c=y)
     plt.plot(history['train_loss'], label='train_loss')
     plt.plot(history['valid_loss'], label='valid_loss')
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
     plt.legend()
+    plt.grid(True)
     plt.show()